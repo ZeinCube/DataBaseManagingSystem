de6c59c969a2d0df0b2f74190b4cc9a98f0eceae
MSI <perun042199@gmail.com>
2019-10-01 19:34:46 +1000

    Fix .iml file

--- Engine/DBEngine.java
+++ Engine/DBEngine.java
@@ -1,13 +1,23 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class DBEngine {
+    private final String homeDirectory = System.getProperty("user.home") + "/.dbms";
 
     public void createDataBase(String name) {
 
     }
 
-    public void checkFiles(String name) {
-        File directory = new File(System.getProperty("user.home") + "/.dbms");
+    public void checkFiles(String name) throws FileNotFoundException {
 
+        File directory = new File(homeDirectory);
+
+        if (!directory.exists()) {
+            if (!directory.mkdir()) {
+                throw new FileNotFoundException("Could not create directory");
+            }
+        }
+
+        File metaFile = new File(homeDirectory + "/");
     }
 }
