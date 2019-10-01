261f2a5fca7ac0809ef49da4bf480cf265c7852d
Ivan Kokorev <perun042199@gmail.com>
2019-10-01 14:13:19 +1000

    Make console main class (#5)

--- Engine/DBEngine.java
+++ Engine/DBEngine.java
@@ -1,6 +1,13 @@
+import java.io.File;
+
 public class DBEngine {
-    
-    public static void createDB(String name) {
+
+    public void createDataBase(String name) {
+
+    }
+
+    public void checkFiles(String name) {
+        File directory = new File(System.getProperty("user.home") + "/.dbms");
 
     }
 }
