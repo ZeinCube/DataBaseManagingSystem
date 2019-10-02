66f7d75a1d3221297b6df4e151594e23e07dcdd8
Alex <sashadz2002@gmail.com>
2019-10-03 01:17:56 +1000

    Кнопки добавлены + папка с тестами

--- Tester/src/controller/MainController.kt
+++ Tester/src/controller/MainController.kt
@@ -1,16 +1,16 @@
 package controller
 
 import javafx.fxml.FXML
-import javafx.scene.control.Button
-import javafx.scene.control.TreeTableView
+import javafx.scene.control.*
 import javafx.scene.layout.Pane
+import javafx.scene.control.cell.TreeItemPropertyValueFactory
+import java.io.File
+import java.nio.file.Paths
 import javafx.scene.control.TreeItem
 import javafx.scene.control.TreeTableColumn
-import javafx.scene.control.cell.TreeItemPropertyValueFactory
-
-
-
-
+import com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table
+import javafx.event.ActionEvent
+import javafx.util.Callback
 
 /**
  * Created by naik on 06.02.16.
@@ -20,46 +20,106 @@ class MainController {
     @FXML
     lateinit var testButton: Button
     lateinit var MainPlane: Pane
-    fun initialize() {
-        print("Controller working")
-        testButton.setOnAction {
-            print("Button clicked!")
+    lateinit var newGroupeName: TextArea
+
+    var testGropes: Array<GroupeOfTests> = arrayOf()
+    var _path =Paths.get("").toAbsolutePath().toString()
+    var path = "$_path\\tests\\"
+    val mainDir = File( "$path\\meta.txt")
+    val treeTableView = TreeTableView<Test>()
+
+    private fun addButtonToTable() {
+        val colBtn = TreeTableColumn<Test, Void>("Button Column")
+
+        val cellFactory = Callback<TreeTableColumn<Test, Void>, TreeTableCell<Test, Void>> {
+            object : TreeTableCell<Test, Void>() {
+
+                private val btn = Button("Action")
+
+                init {
+                    btn.setOnAction { event: ActionEvent ->
+                        val data = treeTableView.getTreeItem(index)
+                        println("selectedData: $data")
+                    }
+                }
+
+                override fun updateItem(item: Void?, empty: Boolean) {
+                    super.updateItem(item, empty)
+                    if (empty) {
+                        graphic = null
+                    } else {
+                        graphic = btn
+                    }
+                }
+            }
         }
-        val treeTableView = TreeTableView<TestResult>()
 
-        // Create column EmpNo (Data type of String).
-        val test //
-                = TreeTableColumn<TestResult, String>("Tests")
-        test.minWidth = 100.0
-        // Create column FullName (Data type of String).
-        val result//
-                = TreeTableColumn<TestResult, String>("Result")
-        treeTableView.columns.addAll(test,result)
+        colBtn.setCellFactory(cellFactory)
 
-        test.setCellValueFactory(TreeItemPropertyValueFactory<TestResult, String>("name"))
-        result.setCellValueFactory(TreeItemPropertyValueFactory<TestResult, String>("result"))
+        treeTableView.columns.add(colBtn)
 
-        val gr1 = TestResult("G0","OK")
-        val t1 = TestResult("Test0","OK")
-        val ti0 = TreeItem<TestResult>(gr1)
-        val ti1 = TreeItem<TestResult>(t1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        ti0.children.addAll(ti1)
-        val root = TreeItem<TestResult>(TestResult("","OK"));
-        root.children.addAll(ti0)
-        root.children.addAll(ti0)
-        root.children.addAll(ti0)
-        root.children.addAll(ti0)
+    }
+
+    fun updateTable()
+    {
+        testGropes = arrayOf()
+        mainDir.forEachLine {
+            testGropes = testGropes + arrayOf(GroupeOfTests("$path",it))
+        }
+
+        val testnamec = TreeTableColumn<Test, String>("Tests")
+        testnamec.minWidth = 100.0
+        val resultc = TreeTableColumn<Test, String>("Result")
+
+        treeTableView.columns.addAll(testnamec,resultc)
+        addButtonToTable()
+        treeTableView.setOnMouseClicked(){
+
+                if(it.getClickCount() == 2){
+                    println(it)
+                }
+        }
+
+
+
+        testnamec.setCellValueFactory(TreeItemPropertyValueFactory<Test, String>("name"))
+        resultc.setCellValueFactory(TreeItemPropertyValueFactory<Test, String>("result"))
+
+
+
+
+        val root = TreeItem<Test>(Test(" ", "OK"));
+        for (gr in testGropes)
+        {
+            val subRoot = TreeItem<Test>(gr.name)
+            for (test in gr.Tests)
+            {
+                subRoot.children.add(TreeItem(test))
+            }
+            root.children.add(subRoot)
+        }
+
         treeTableView.root = root
+    }
+    fun initialize() {
+
+
+        updateTable()
+        testButton.setOnAction {
+            val name = newGroupeName.text
+            val newDir = File("$path$name")
+            mainDir.appendText("\n$name")
+            newDir.mkdir()
+            val newFile = File("$path$name\\meta.txt")
+            newFile.createNewFile()
+            updateTable()
+        }
+
+
+
         MainPlane.children.add(treeTableView)
     }
 
 }
+
+
