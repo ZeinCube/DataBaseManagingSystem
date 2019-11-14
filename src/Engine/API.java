90af56cdf0f871dabeda080200d9107eb66a6bcf
ZeinCube <perun042199@gmail.com>
2019-11-14 15:43:21 +1000

    Fix drop table

--- src/Engine/API.java
+++ src/Engine/API.java
@@ -56,6 +56,8 @@ public class API {
             throw new DropException("Table " + tableName + "could not be dropped cause :" + e.getMessage());
         }
 
+        commit(metaFile);
+
         return "Table " + tableName + " dropped";
     }
 
