657a53a75f2bd1ea3090a3d435522996e3496ddb
dianaRusak <dianka23@inbox.ru>
2019-11-14 15:48:07 +1000

    add system messages

--- src/Logic/Listener.java
+++ src/Logic/Listener.java
@@ -96,7 +96,9 @@ public class Listener extends HelloBaseListener {
                     String string = ctx.name(i - 1).getText();
                     api.dropTable(string);
                 } catch (DropException e) {
-                    e.printStackTrace();
+                    System.err.println(e.getMessage());
+                } finally {
+                    System.out.println("table droped");
                 }
                 i--;
             }
@@ -107,7 +109,7 @@ public class Listener extends HelloBaseListener {
                 try {
                     System.out.println(api.showCreateTable(string));
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    System.err.println(e.getMessage());
                 }
                 i--;
             }
@@ -149,7 +151,9 @@ public class Listener extends HelloBaseListener {
             String kek = hashMap.get("Table_name").toString();
             api.createTable(kek, hashSet);
         } catch (DBMSException e) {
-            e.printStackTrace();
+            System.err.println(e.getMessage());
+        } finally {
+            System.out.println("table created");
         }
     }
 }
