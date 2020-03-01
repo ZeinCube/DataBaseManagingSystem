d72651457be23fb23850d54acf7ecb5b6fb88fca
Anton Melnikov <hehogcode@gmail.com>
2020-03-01 14:34:58 +1000

    fix build maven modules

--- TestModule/src/main/java/Test/Main.java
+++ TestModule/src/main/java/Test/Main.java
@@ -1,10 +1,12 @@
 package Test;
 
 import Test.Engine.TestCli;
+import Logic.ParserManager;
 
 public class Main {
 
     public static void main(String[] args) {
+        ParserManager manager = new ParserManager();
         TestCli cli = new TestCli();
         cli.run();
     }
